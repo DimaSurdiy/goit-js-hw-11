e9ad13b87f28367c8bdeff20e678c0c79f72c6ab
DimaSurdiy <dsurdiy@ukr.net>
2022-07-10 07:29:58 +0300

    Add axios & async/await

--- src/js/images-service.js
+++ src/js/images-service.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '25683514-2bb572422bae18aceebfa826b';
 
@@ -7,14 +9,17 @@ export default class ImagesApiService {
     this.page = 1;
   }
 
-  fetchImages() {
+  async fetchImages() {
     const parameters = `q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
     const url = `${BASE_URL}?key=${API_KEY}&${parameters}`;
 
-    return fetch(url).then(res => {
+    try {
+      const response = await axios.get(url);
       this.page += 1;
-      return res.json();
-    });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   get query() {
